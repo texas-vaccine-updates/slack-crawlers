b044ac6fc620528662431a34cfff913f34f67a34
jameskip <me@jameskip.com>
2021-01-31 21:10:16 -0600

    test

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ cron.schedule(cronJobInterval, () => {
 
     for (const location in vaccineLocations.locations) {
       const openTimeslot = vaccineLocations.locations[location].openTimeslots;
-      if (openTimeslot !== 0) {
+      if (openTimeslot === 0) {
         await webhook.send(slackMessageBlock);
         return;
       }
