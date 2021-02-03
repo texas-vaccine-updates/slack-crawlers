09a58686e7719f4a9ca8a3aa69a7552a23ec7bb6
jameskip <me@jameskip.com>
2021-02-02 18:14:29 -0600

    Add tests

---
+++ tests/heb.test.js
@@ -0,0 +1,12 @@
+// const checkHeb = require('../crawlers/heb');
+
+describe('should fetch HEB data', () => {
+  // TODO: Write actual tests
+  it('should send slack message when vaccines are available', () => {
+    expect('poop').toBe('poop');
+  });
+
+  it('should not send slack message when vaccines are not available', () => {
+    expect('poop').toBe('poop');
+  });
+});
