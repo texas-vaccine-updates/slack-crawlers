e09fb80875562a8bfc07bf97802874096a3b2c90
jameskip <me@jameskip.com>
2021-02-13 11:52:22 -0600

    Add new cookie and nocache

--- crawlers/university.js
+++ crawlers/university.js
@@ -4,7 +4,6 @@ const {IncomingWebhook} = require('@slack/webhook');
 const renderStaticSlackMessage = require('../utils/renderStaticSlackMessage');
 const getDateString = require('../utils/getDateString');
 const universityURL = 'https://mychart-openscheduling.et1130.epichosted.com/MyChart/SignupAndSchedule/EmbeddedSchedule?id=51748&dept=10554003&vt=1788&view=grouped';
-const universityAPI = 'https://mychart-openscheduling.et1130.epichosted.com/MyChart/OpenScheduling/OpenScheduling/GetOpeningsForProvider';
 
 dotenv.config();
 
@@ -14,9 +13,12 @@ const webhook = new IncomingWebhook(url);
 const dateString = getDateString();
 const futureDateString = getDateString(14);
 
+const noCache = Math.random();
+const universityAPI = `https://mychart-openscheduling.et1130.epichosted.com/MyChart/OpenScheduling/OpenScheduling/GetOpeningsForProvider?noCache=${noCache}`;
+
 const options = {
   'headers': {
-    '__requestverificationtoken': 'S5HgmjIeQCPvhr4PHa9Dv2YJZoyDALC0S8vG-HjIPtiTy-T998PvHO4l1rLZIXj2eBKDtcY9SIF0SnY8VGHbRgJutic1',
+    '__requestverificationtoken': 'ojvbzlc_vzThMdoVjwzqyftvLchb1vVyrDgCdbB5Ck8ZjUuLekGdl22J-VIDbRoZycsJ3qb0X3PymC-yXFeFUy-0oas1',
     'accept': '*/*',
     'accept-language': 'en-US,en;q=0.9',
     'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
@@ -24,7 +26,7 @@ const options = {
     'sec-fetch-mode': 'cors',
     'sec-fetch-site': 'same-origin',
     'x-requested-with': 'XMLHttpRequest',
-    'cookie': 'MyChartLocale=en-US; MyChart_Session=up3qqy3z4g000ag4zkiom2dy; __RequestVerificationToken_L015Q2hhcnQ1=Tms0Jhmklly2XnnN_Lnwn0gpQF5Or_UxtnHEGICQloUNUlPO-AYoYBCt0dVC6k3S8cZw76HvjDboUZOOWkzsMBjjHrQ1; MYCPERS=3456112300.47873.0000; .WPCOOKIE4mychart=52AFEC86E73628B7E97E510E969249F8A1FCBCE4FB3D4895DAB28798B876148EE92C0431CE1FAC370221AC0B13E743C7DFE999F6E83068A627A8A3DAF08587BDFC5DAD674EFC2600BFC993AA1469E8CC0138B1E6FFDCDAF098EEF07EC5F3FFE7E9343097DF96A953BA0A0344808D4833D0E72290880C88C4B08BF541924F56B050C124EDC48EFF4FBF740C15E7C9BF0B2BBD74540A91E928C5F4714871160BE1F30830A3E8ED7730C3D323D08FEE814C0F7A3E8E71485F5A49A22383465E83179E44D5F118AD78A055E1CC0599B3C2E5F27F7D56EC023CD612A9123C9527E7E407186EDCB4E1698BBFEF1CB814D3DA703B793591',
+    'cookie': 'MyChartLocale=en-US; MyChart_Session=up3qqy3z4g000ag4zkiom2dy; MYCPERS=3456112300.47873.0000; __RequestVerificationToken_L015Q2hhcnQ1=Sz4alFszHZWRKl9SXeLvewjgpH_MhIZ7JDfys3xgsR1xKxKI-72ClOZcdjtGRF9rD-4QIIHFMMNztXFchtBtYUVy1RY1; .WPCOOKIE4mychart=8789544DDE0B261D8E1B529C23279091E32C2A05AC0E1631C767AA352522EB6F64E25C43D2CB9FAE37D334F29ABC52C6CD674B5557A6C5AF2E828EAF87D151D46B19FE8EC1A8339B59AAD4AC83C42CC422146DC40EDBC0D3D02E5B2C92B0294026AF72E95C988131C4705EB34EBE9B4032FC33740A88E65822EC97EB50AC4FCCBD48F0911BD4562634FE9ACEA5CF767A490386C0D0A9D9340DE6410E523F8B59E7D465F17701A99DB319832CA2430853D309C321E6E222B8EDF6B77F271B8F817832CB154F48E14B8A20D15B1F1E42575D4C3E045745F1FFB86D305AB0BA2B1123176EDCED92E1A9242C72AF9A0C15AB0FA3979F',
   },
   'referrer': 'https://mychart-openscheduling.et1130.epichosted.com/MyChart/SignupAndSchedule/EmbeddedSchedule?id=51748&dept=10554003&vt=1788&view=grouped',
   'referrerPolicy': 'same-origin',
@@ -67,5 +69,6 @@ const checkUniversity = async () => {
     console.error(e);
   }
 };
+checkUniversity();
 
 module.exports = checkUniversity;
