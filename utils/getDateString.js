3beb151c1ff497db7af4f8ad1100bc2c6c492555
jameskip <me@jameskip.com>
2021-02-26 11:47:49 -0600

    update addeddays

--- utils/getDateString.js
+++ utils/getDateString.js
@@ -1,6 +1,8 @@
 module.exports = (addedDays = 0) => {
   const date = new Date();
-  date.setDate(date.getDate() + addedDays);
+  if (addedDays > 0) {
+    date.setDate(date.getDate() + addedDays);
+  }
   const day = ('0' + date.getDate()).slice(-2);
   const month = ('0' + (date.getMonth() + 1)).slice(-2);
   const year = date.getFullYear();
