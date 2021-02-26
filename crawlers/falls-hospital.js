d05fe3340bd8de032b9d41de68fb753da7a717b5
jameskip <me@jameskip.com>
2021-02-26 13:05:01 -0600

    Change interval

--- crawlers/falls-hospital.js
+++ crawlers/falls-hospital.js
@@ -48,7 +48,7 @@ const checkFallsHospital = async () => {
 
     if (response.status === 200) {
       const thisTime = data.data.length;
-      if (thisTime > (lastTime + 40) || thisTime < (lastTime - 40)) {
+      if (thisTime > (lastTime + 25) || thisTime < (lastTime - 25)) {
         lastTime = data.data.length;
         await webhook.send(renderFallsSlackMessage(scheduleURL, lastTime));
       }
