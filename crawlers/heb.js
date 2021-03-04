4214dff3a08ada81d62b45b5e9487aca012d7fce
jameskip <me@jameskip.com>
2021-03-04 12:08:42 -0600

    Set up change parameters to enable faster interval

--- crawlers/heb.js
+++ crawlers/heb.js
@@ -11,16 +11,7 @@ dotenv.config();
 const webhookURL = process.env.HEB_WEBHOOK_URL;
 const webhook = new IncomingWebhook(webhookURL);
 
-const excludedStores = [
-  'Lubbock H-E-B',
-  'H-E-B Pharmacy at the Medical Center Hospital',
-  'Huntsville H-E-B',
-  'Midland H-E-B',
-  'Midland Loop 250 H-E-B',
-  'COVID 19 Vaccination Clinic at Waxahachie Baptist Church',
-  'University Blvd. H-E-B',
-  'COVID 19 Vaccination Clinic at Old Kerr Middle School Campus',
-];
+let lastRunSlotCount = [];
 
 const checkHeb = async () => {
   try {
@@ -28,47 +19,54 @@ const checkHeb = async () => {
     const response = await fetch(hebURL);
     const vaccineLocations = await response.json();
 
-    if (response.status === 200) {
-      const locationsWithVaccine = {};
+    if (lastRunSlotCount.length === 0) {
+      lastRunSlotCount = vaccineLocations['locations'];
+    }
 
-      for (location in vaccineLocations.locations) {
-        if (vaccineLocations.locations.hasOwnProperty(location)) {
-          const {name, openTimeslots, city, street, url} = vaccineLocations.locations[location];
+    const locationsWithVaccine = {};
 
-          if (openTimeslots > 4 && !excludedStores.includes(name)) {
-            locationsWithVaccine[name] = {openTimeslots, city, url, street};
-          }
+    for (location in vaccineLocations.locations) {
+      if (vaccineLocations.locations.hasOwnProperty(location)) {
+        const {name, openTimeslots, city, street, url} = vaccineLocations.locations[location];
+
+        if (openTimeslots > 3) {
+          locationsWithVaccine[name] = {name, openTimeslots, city, url, street};
         }
       }
+    }
 
-      if (Object.keys(locationsWithVaccine).length === 0) {
-        return;
-      }
+    if (Object.keys(locationsWithVaccine).length === 0) {
+      return;
+    }
 
-      const slackFields = [];
+    const slackFields = [];
 
-      for (location in locationsWithVaccine) {
-        if (locationsWithVaccine.hasOwnProperty(location)) {
-          const {openTimeslots, city, url, street} = locationsWithVaccine[location];
-          const capatilizedCity = capitalizeSentance(city);
-          const urlFriendlyAddress = `${street.split(' ').join('+')}+${city.split(' ').join('+')}`;
+    for (location in locationsWithVaccine) {
+      if (locationsWithVaccine.hasOwnProperty(location)) {
+        const {openTimeslots, city, url, street, name} = locationsWithVaccine[location];
+        const capatilizedCity = capitalizeSentance(city);
+        const urlFriendlyAddress = `${street.split(' ').join('+')}+${city.split(' ').join('+')}`;
+        const lastFound = lastRunSlotCount.find((locale) => locale.name === name);
 
+        if (openTimeslots > (lastFound.openTimeslots + 3)) {
           slackFields.push({
             type: 'mrkdwn',
             text: `<${url || hebURL}|${location}>:  *${openTimeslots}* \n<https://google.com/maps/?q=${urlFriendlyAddress}|${capatilizedCity}>`,
           });
         }
       }
-
-      if (slackFields.length > 10) {
-        slackFields.length = 10; // Slack limits the number of fields to 10
-      }
-
-      if (slackFields.length > 0) {
-        const slackMessage = renderSlackMessage(scheduleURL, slackFields);
-        await webhook.send(slackMessage);
-      }
     }
+
+    if (slackFields.length > 10) {
+      slackFields.length = 10; // Slack limits the number of fields to 10
+    }
+
+    if (slackFields.length > 0) {
+      const slackMessage = renderSlackMessage(scheduleURL, slackFields);
+      await webhook.send(slackMessage);
+    }
+
+    lastRunSlotCount = vaccineLocations['locations'];
   } catch (e) {
     console.error(e);
   }
