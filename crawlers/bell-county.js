5414d29a520ec91292d89e16e8cbfa5e6cf03cd2
jameskip <me@jameskip.com>
2021-03-07 09:53:54 -0600

    Update fetch options

--- crawlers/bell-county.js
+++ crawlers/bell-county.js
@@ -23,10 +23,10 @@ const killeenOptions = {
     'sec-fetch-dest': 'empty',
     'sec-fetch-mode': 'cors',
     'sec-fetch-site': 'same-origin',
-    'cookie': 'ClientId=BE2D9ACA38F24040B4F9F9A3A37B3159; OIDC=1; OutlookSession=62f718dc95d94dfba2817aabd4cdb90f',
+    'cookie': 'ClientId=BE2D9ACA38F24040B4F9F9A3A37B3159; OIDC=1; OutlookSession=890eac6c2e6746a0a8b8a8ec32d7f437',
   },
   'referrerPolicy': 'no-referrer',
-  'body': '{"StaffList":["0K2vfoBvR0Gxqhjo3CzMog=="],"Start":"2021-02-28T00:00:00","End":"2021-05-01T00:00:00","TimeZone":"America/Chicago","ServiceId":"06oo6bJVYUmbCVQMl9fGmA2"}',
+  'body': '{"StaffList":["0K2vfoBvR0Gxqhjo3CzMog=="],"Start":"2021-03-07T00:00:00","End":"2021-05-02T00:00:00","TimeZone":"America/Chicago","ServiceId":"06oo6bJVYUmbCVQMl9fGmA2"}',
   'method': 'POST',
   'mode': 'cors',
 };
