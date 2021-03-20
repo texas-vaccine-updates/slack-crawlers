b53d6f48a6931e947ea9b69995fc358899838e2d
jameskip <me@jameskip.com>
2021-03-20 10:53:34 -0500

    hotfix

--- index.js
+++ index.js
@@ -10,8 +10,10 @@ const checkUniversity = require('./crawlers/university');
 const checkFallsHospital = require('./crawlers/falls-hospital');
 // const checkCoryell = require('./crawlers/coryellhealth');
 const checkWalmart = require('./crawlers/walmart');
+const checkWalgreens = require('./crawlers/walgreens')
 
 const dotenv = require('dotenv');
+const checkWalgreens = require('./crawlers/walgreens');
 dotenv.config();
 
 const cronJobInterval = '*/1 * * * *';
@@ -33,10 +35,11 @@ cron.schedule(cronJobInterval, async () => {
     await checkBellCounty();
     await checkAlamodome();
     await checkUniversity();
-    await checkFallsHospital();
     // await checkRandalls(); NOTE: Currently being blocked by Albertsons
     // await checkCoryell(); TODO: FIX: 400 requests
     await checkWalmart();
+    await checkWalgreens();
+    await checkFallsHospital();
   } catch (error) {
     console.error(error);
   }
